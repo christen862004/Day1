e872db206cdc8f85cacb7f99dc8a0ffdc82b6242
Christen <christen.foad@gmail.com>
2024-02-24 12:33:18 +0200

    Add project files.

---
+++ Day1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace Day1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello FRom WPF ");
+        }
+
+        private void Button_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            MessageBox.Show("Hello FRom WPF ");
+
+        }
+    }
+}
